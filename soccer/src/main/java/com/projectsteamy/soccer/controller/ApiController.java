64829b43477fa638585bc4185129ee3a27cf22a3
Elizabeth Lim <elizabethlim43@gmail.com>
2020-04-11 11:24:26 -0500

    Adding all json files additional example call

--- soccer/src/main/java/com/projectsteamy/soccer/controller/ApiController.java
+++ soccer/src/main/java/com/projectsteamy/soccer/controller/ApiController.java
@@ -3,11 +3,15 @@ package com.projectsteamy.soccer.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
     private String word = "";
+    private List<String> players = new ArrayList<>();
 
     //yell
     @RequestMapping("/yell")
@@ -21,5 +25,13 @@ public class ApiController {
     public String dontyell() {
         word = "soccer...";
         return word;
+    }
+
+    @RequestMapping("/players")
+    public List<String> getPlayers() {
+        players.add("Messi");
+        players.add("Ronaldo");
+        players.add("Neymar");
+        return players;
     }
 }
